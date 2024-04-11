996c8cce7748795061ff056890e951e7c4550443
RodrigoSHL <shl.catalan@gmail.com>
2024-04-11 15:49:46 -0400

    FEAT: install emotion/react and emotion/styles to create 'styled component'
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,41 @@
 import { useState } from "react"
 import './Counter.css'
+import styled from "@emotion/styled"
+
+const BtnDiminish = styled.button`
+  color: tomato;
+  background-color: black;
+  
+  &:hover {
+    background-color: tomato;
+    color: black;
+    font-weight: bold;
+  }`
 
 const Counter = () => {
 
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-    const handleCount = () => {
-      setCount(count + 1)
-    }
+  const handleCount = () => {
+    setCount(count + 1)
+  }
 
-    const handleCountReset = () => {
-      setCount(0)
-    }
+  const handleMin = () => {
+    setCount(count - 1)
+  }
+
+  const handleCountReset = () => {
+    setCount(0)
+  }
   return (
-    <>
-    <h1>Counter: {count}</h1>
-    <button onClick={handleCount}>Click me</button>
-    <button style={{margin: '1rem'}} onClick={handleCountReset}>Reset</button>
+    <div>
+      <h1>Counter: {count}</h1>
+      <BtnDiminish onClick={handleCount}>+</BtnDiminish>
+      <button style={{ margin: '1rem', backgroundColor: 'tomato' }} onClick={handleCountReset}>Reset</button>
+      <BtnDiminish onClick={handleMin}>-</BtnDiminish>
 
-    </>
-    
+    </div>
+
   )
 }
 
